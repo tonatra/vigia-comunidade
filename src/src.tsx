006183bba030328fc969084776edcf9aa8d02ad4
tonatra <119815267+tonatra@users.noreply.github.com>
2025-10-10 09:52:03 -0300

    Create src.tsx

---
+++ src/src.tsx
@@ -0,0 +1,11 @@
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+L.Icon.Default.mergeOptions({
+  iconUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconRetinaUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  shadowUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+});
